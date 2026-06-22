360f4cad29226e97f30e34e5c8f3a46c11834c4f
Muhammad Arfasya <arfasyarespati@gmail.com>
2026-06-22 15:57:04 +0700

    adding: buttons

--- MyWebsite/react/my-react-app/src/App.jsx
+++ MyWebsite/react/my-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/Button.jsx";
 
 function App() {
  
@@ -15,8 +16,12 @@ function Header(){
       <span className="text-zinc-400 text-sm">1 / 1 done today</span>
     </div>
 
-    <div>
-
+    <div className="flex flex-col gap-1">
+      <span className="text-zinc-400 text-sm">June 22 - July 22</span>
+      <div className="flex items-center gap-3"> 
+        <Button>Prev</Button>
+        <Button>Next</Button>
+      </div>
     </div>
   </header>
 }
