bc64f7c2846a73b02548b88ec69330eced10b8fd
Muhammad Arfasya <arfasyarespati@gmail.com>
2026-07-01 17:45:41 +0700

    learn: js array fundamentals

--- testing/Javascript/backend-oop.js
+++ testing/Javascript/backend-oop.js
@@ -1,1 +1,35 @@
-console.log("woi")
+let activity = ["Learning React", "Learning Typescript", "Play Rimworld"]
+
+function showActivity(){
+    console.log("Activites:")
+    if (activity === 0) {
+        console.log("No activites yet!!")
+    } else {
+        activity.forEach((name, index) => {
+            console.log(`${index + 1}. ${name}`)
+        })
+    }
+}
+
+function addActivity(newActivity){
+    activity.push(newActivity)
+    console.log(`Succesfully added ${newActivity}!`)
+    showActivity()
+}
+
+function deleteActivity(number){
+    let index = number - 1;
+    if (index >= 0 && index < activity.length) {
+        let deleted = activity.splice(index, 1);
+        console.log(`Succesfully deleted number ${number}!`)
+    } else {
+        console.log("Unknown number!!")
+    } 
+    
+    showActivity()
+}
+
+showActivity()
+addActivity("Scroll Instagram")
+deleteActivity(4)
+deleteActivity(4)
